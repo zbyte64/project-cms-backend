791f65a9d817ed805eafe484224d5fc176ef701b
Jason Kraus <zbyte64@gmail.com>
2016-09-28 14:41:38 -0700

    function to initialize userdata table, use secondary index for more efficient querying, fix id computation to include tableName

--- src/connections.js
+++ src/connections.js
@@ -1,18 +1,38 @@
-var stripe = require('stripe');
+const stripe = require('stripe');
+const _ = require('lodash')
 
 //TODO process.env configurable
-var r = require('rethinkdbdash')({
+const r = require('rethinkdbdash')({
   port: 29015,
   host: 'rethinkdb',
 });
 exports.r = r;
+
+const USER_TABLE_INDEX = 'userAndTable';
+exports.USER_TABLE_INDEX = USER_TABLE_INDEX;
+
+function init_database() {
+  return r.tableList().run().then(tableNames => {
+    if (_.indexOf(tableNames, 'userdata') !== -1) {
+      //pass
+    } else {
+      return r.createTable('userdata').run().then(success => {
+        return r.table('userdata').createIndex(USER_TABLE_INDEX, [r.row('_user'), r.row('_tableName')]).run()
+      })
+    }
+  }).catch(error => {
+    console.log("Error initializing database:");
+    console.error(error);
+  });
+}
+init_database();
 
 function makeStripeClient() {
   //read credentials from environ
   return stripe(process.env.STRIPE_API_KEY);
 }
 
-var stripeClient = makeStripeClient();
+const stripeClient = makeStripeClient();
 
 exports.makeStripeClient = makeStripeClient;
 exports.stripeClient = stripeClient;
